4849af43b3b74d8c709ef1673df9ffac4a3e8972
durgesh730 <durgesh.unicoglobal@gmail.com>
2025-01-12 19:16:06 +0530

    set isloading false

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -19,44 +19,53 @@ export const AuthProvider = ({ children }) => {
     const [loader, setLoader] = useState(true);
     const [CMSData, setCMSData] = useState({})
 
-    useEffect(() => {
-        CMSServices.getCMS().then((res) => {
-            setCMSData(res.data)
-            document.documentElement.style.setProperty('--brand-color', res.data.colors.brand);
-            document.documentElement.style.setProperty('--brand-pastel-color', res.data.colors.pastel);
-            document.documentElement.style.setProperty('--brand-grey-color', res.data.colors.grey);
-            document.documentElement.style.setProperty('--brand-white-color', res.data.colors.white);
+    const getUIChanges = () => {
+        CMSServices.getCMS()
+            .then((res) => {
+                setCMSData(res.data)
+                document.documentElement.style.setProperty('--brand-color', res.data.colors.brand);
+                document.documentElement.style.setProperty('--brand-pastel-color', res.data.colors.pastel);
+                document.documentElement.style.setProperty('--brand-grey-color', res.data.colors.grey);
+                document.documentElement.style.setProperty('--brand-white-color', res.data.colors.white);
 
-            // admin text color 
-            document.documentElement.style.setProperty('--admin-color-one', res.data.admin.secondary);
-            document.documentElement.style.setProperty('-admin-color-two', res.data.admin.tertiary);
-            setLoader(false)
-            localStorage.setItem("logo", res.data.logo)
-        }).catch((err) => {
-            console.log(err)
-        })
+                // admin text color 
+                document.documentElement.style.setProperty('--admin-color-one', res.data.admin.secondary);
+                document.documentElement.style.setProperty('-admin-color-two', res.data.admin.tertiary);
+
+                localStorage.setItem("logo", res.data.logo)
+            }).finally(() => {
+                setLoader(false)
+            })
+    }
+
+    useEffect(() => {
+        getUIChanges()
     }, [])
 
     const fetchUser = async () => {
         try {
-            let data = await TeamMemberService.getMe()
-                .catch((err) => alert.SnackbarHandler(true, "error", err.response?.data?.data || "An error occurred"));
+            await TeamMemberService.getMe()
+                .then((res) => {
+                    if (!res.success) return;
+                    setUser(res.data.user);
+                    setrolesAndPermissions(res.data.rolesAndPermissions);
+                })
+                .catch((err) => {
+                    alert.SnackbarHandler(true, "error", err.response?.data?.data || "An error occurred")
+                }).finally(() => {
+                    setLoader(false)
+                })
 
-            if (!data.success) return;
-
-            setUser(data.data.user);
-            setrolesAndPermissions(data.data.rolesAndPermissions);
         } catch (error) {
             console.error("Error fetching user data:", error);
         }
     };
 
     useEffect(() => {
-        // Only fetch user data if it hasn't been fetched before
         if (!user) {
             fetchUser();
         }
-    }, [user]); // Fetch user data whenever the "user" state change
+    }, [user]);
 
     const value = {
         CMSData, user, setUser, loader,
